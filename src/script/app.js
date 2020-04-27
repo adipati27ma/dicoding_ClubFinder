0bdb8fb593011e05a463ca82700fd981dc7d8376
adipati27ma <adipati.cs.samsung@gmail.com>
2020-04-28 00:11:34 +0700

    APP JS BODOH

--- src/script/app.js
+++ src/script/app.js
@@ -1,6 +1,6 @@
 import "regenerator-runtime";
 import "./styles/style.css";
-import "./script/component/app-bar.js";
-import main from "./script/view/main.js";
+import "./component/app-bar.js";
+import main from "./view/main.js";
 
 document.addEventListener("DOMContentLoaded", main);
